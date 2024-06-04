f1f28c506cb44030c110fb0876b93461cef882f4
rahul00221 <rahul.gadi@50hertz.in>
2024-06-04 17:59:51 +0530

    Add files via upload

---
+++ dragdrop/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Reorder from './cmp/Reorder';
+
+function App() {
+  return (
+    <div className="App">
+      <Reorder/>
+    </div>
+  );
+}
+
+export default App;
---
+++ dragdrop/src/cmp/Reorder.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
+
+const getItems = count =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    content: `item ${k}`
+  }));
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  userSelect: "none",
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+  background: isDragging ? "lightgreen" : "grey",
+  ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? "lightblue" : "lightgrey",
+  padding: grid,
+  width: 250
+});
+
+class Reorder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: getItems(12)
+    };
+  }
+
+  onDragEnd = result => {
+    if (!result.destination) {
+      return;
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items
+    });
+  };
+
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+              style={getListStyle(snapshot.isDraggingOver)}
+            >
+              {this.state.items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      style={getItemStyle(
+                        snapshot.isDragging,
+                        provided.draggableProps.style
+                      )}
+                    >
+                      <span index={index}>{item.content}</span>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    );
+  }
+}
+
+export default Reorder;
